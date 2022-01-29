85adf9e2239284671daebc3dae2be9d0064ac887
SajadSj <sajadsojoudi@gmail.com>
2022-01-29 22:41:51 +0330

    api example

--- social/settings.py
+++ social/settings.py
@@ -139,5 +139,5 @@ CORS_ALLOWED_ORIGINS = [
     # "https://example.com",
     # "https://sub.example.com",
     "http://localhost:8080",
-    "http://127.0.0.1:9000",
+    "http://127.0.0.1:8000",
 ]
